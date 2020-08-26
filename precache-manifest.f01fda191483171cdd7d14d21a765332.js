3464ef67c4a2e9fd376b60ea60ba2dda006d74e5
kunjan tanna <kvtanna.om@gmail.com>
2020-08-26 11:23:23 +0530

    Updates

---
+++ precache-manifest.f01fda191483171cdd7d14d21a765332.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a682c0ca007a9c708d0b00ab8c72a6e",
+    "url": "/basic-proj/index.html"
+  },
+  {
+    "revision": "1ec7c3055617c88d1034",
+    "url": "/basic-proj/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "48b27317c8c0baeaf2fd",
+    "url": "/basic-proj/static/js/2.d069fd99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/basic-proj/static/js/2.d069fd99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ec7c3055617c88d1034",
+    "url": "/basic-proj/static/js/main.43be1e60.chunk.js"
+  },
+  {
+    "revision": "09c9c182661bd66ded45",
+    "url": "/basic-proj/static/js/runtime-main.64cd1745.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/basic-proj/static/media/logo.5d5d9eef.svg"
+  }
+]);
